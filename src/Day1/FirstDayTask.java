c146393901eccb91ca68b87c4a81ca8d3ed73767
tosunumutt <tosun.umutt@gmail.com>
2024-05-26 22:20:01 +0300

    First Day Task Completed 26.05.2024

--- src/Day1/FirstDayTask.java
+++ src/Day1/FirstDayTask.java
@@ -2,6 +2,26 @@ package Day1;
 
 public class FirstDayTask {
     public static void main(String[] args) {
+            String district = "Kartal";
+            String continent = "europe";
+            String fax = "tosun.umutt@fax.com";
+            String postCode = "462123";
+            String instagramUrl = "tosunumutt";
+            String workDepartman = "IT";
+            String customerSurname = "Tosun";
+            String videoComment = "That was great";
+
+            int productCount = 124;
+        int birthYear = 2004;
+
+            double paymentAmount = 1234.45;
+        double debt = 2437.76;
+
+
+
+
+
+
 
     }
 }
